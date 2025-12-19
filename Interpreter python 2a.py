794b2f4b69704cd4973f212eb55fd0f225ca20e1
Sandra Gryga <sandra.gryga@edu.uekat.pl>
2025-12-19 20:19:50 +0100

    2

---
+++ Interpreter python 2a.py
@@ -0,0 +1,6 @@
+def wyswietlenie_imion(names):
+    for name in names:
+        print(name)
+
+imiona = ["Kasia", "Basia", "Asia", "Jan", "Adam"]
+wyswietlenie_imion(imiona)
---
+++ Interpreter python 2e.py
@@ -0,0 +1,6 @@
+def wyswietlco2(numbers):
+    for num in numbers[0::2]:
+        print(num)
+
+lista = list(range(20))
+wyswietlco2(lista)
